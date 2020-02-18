5fbc9b3a3d75e8e122387a0d684830cb0c74995b
chucknerd <chucknerd1@gmail.com>
2020-02-18 06:03:31 -0600

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Modal from "./Modal.svelte";
+
+  let showModal = false;
+</script>
+
+<style>
+  .input {
+    font-style: italic;
+    opacity: 50%;
+    width: 50%;
+  }
+</style>
+
+<button on:click={() => (showModal = true)}>Forgot Password?</button>
+
+{#if showModal}
+  <Modal on:close={() => (showModal = false)}>
+    <h2>Did you forget your password?</h2>
+    <p>Enter your email below to reset your password.</p>
+    <input class="input" placeholder="Email" required />
+  </Modal>
+{/if}
